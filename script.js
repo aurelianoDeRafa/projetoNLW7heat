ff9589810cfa09fce0d8b9ceea904c5915460f02
rafael16k <leafar.aureliano@gmail.com>
2022-01-21 18:52:27 -0300

    stage4 api do github

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/**um objeto com os nomes da rede social */
 const LinksSocialMedia = {
   github: "aurelianoDeRafa",
   youtube: "test",
@@ -6,15 +7,34 @@ const LinksSocialMedia = {
 }
 
 function changrSocialMediaLinks() {
+  /**vai fazer um loop pegando os filhos do ul(com id=socialLinks) */
   for(let li of socialLinks.children) {
+
+    /**ele vai pega o atributo class e guarda na variavel social */
     const social = li.getAttribute('class')
     
+    /**No html cada (li) tem uma class com nome (youtube, instagram...) */
     li.children[0].href = `https://${social}.com/${
       LinksSocialMedia[social]
     }`
 
-    alert(li.children[0].href)
   }
 }
 
 changrSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name;
+    userBio.textContent = data.bio;
+    userLink.href = data.html_url
+    userPhoto.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGithubProfileInfos()
